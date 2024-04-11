237ec997bc06427a756608e5b2d5492b067b7dcb
jaejun.yang <iam_idea@naver.com>
2024-04-11 06:55:58 +0000

    test 입니다.

--- 20240411_Yangjaejun/prac1_Yangjaejun.py
+++
@@ -1,7 +0,0 @@
-# prac1.py
-
-def say_hello():
-    print("Hello, World!")
-
-if __name__ == "__main__":
-    say_hello()
--- 20240411_Yangjaejun/prac3_Yangjaejun.py
+++
@@ -1,9 +0,0 @@
-# prac3.py
-
-def calculate_average():
-    numbers = [10, 20, 30, 40, 50]
-    average = sum(numbers) / len(numbers)
-    print(f"The average of the numbers {numbers} is {average}.")
-
-if __name__ == "__main__":
-    calculate_average()
