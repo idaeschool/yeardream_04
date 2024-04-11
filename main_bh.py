e2acc9065e3e1e032fa021c4d04f2c6b9ccfa9cb
beomhwan9052 <vajura86@gmail.com>
2024-04-11 08:23:13 +0000

    함범환_온라인 과제제출입니다.

--- main_bh.py
+++ main_bh.py
@@ -3,10 +3,11 @@ import argparse
 import prac1
 import prac2
 import prac3
+import prac6_bh
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3'], help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac6_bh'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -15,6 +16,8 @@ def main():
         prac2.sum_two_numbers()
     elif args.script == 'prac3':
         prac3.calculate_average()
+    elif args.script == "prac6_bh":
+        prac6_bh.game_loop()
 
 if __name__ == "__main__":
     main()
