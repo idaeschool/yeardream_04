ae5d6a8d2902ea5a3b9e855f2d6038a96fa550f3
KJ <virtual96@hanmail.net>
2024-04-11 09:53:03 +0000

    Modified prac4.py + prac5.py & main.py + prac6.py

--- main_M.py
+++ main_M.py
@@ -3,11 +3,13 @@ import argparse
 import prac1
 import prac2
 import prac3
-import prac4
+import prac4_M as prac4
+import prac5
+import prac6
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4'], help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4', 'prac5', 'prac6'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -18,6 +20,10 @@ def main():
         prac3.calculate_average()
     elif args.script == 'prac4':
         prac4.read_file()
+    elif args.script == 'prac5':
+        prac5.main()
+    elif args.script == 'prac6':
+        prac6.game_loop()
 
 if __name__ == "__main__":
     main()
