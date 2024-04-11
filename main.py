dc3c833d87e2f0463f7897efa465645a16f1af5c
winner23456@naver.com <winner23456@naver.com>
2024-04-11 10:08:25 +0000

    Revert "completed"
    This reverts commit 6e41b1674d9d76f067c199a85b33653a52252c52.

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 # main.py
 import argparse
-import prac1 # prac1.py를 불러오겠다! (경로 중요)
-import prac2 # prac2.py를 불러오겠다! (경로 중요)
+import prac1
+import prac2
 import prac3
-import prac4
-import prac5
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script',
-                        choices=['prac1', 'prac2', 'prac3', 'prac4', 'prac5'], 
-                        help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -19,10 +15,6 @@ def main():
         prac2.sum_two_numbers()
     elif args.script == 'prac3':
         prac3.calculate_average()
-    elif args.script == 'prac4':
-        prac4.read_file()
-    elif args.script == 'prac5':
-        prac5.main()
 
 if __name__ == "__main__":
     main()
--- prac5.py
+++ prac5.py
@@ -2,33 +2,17 @@
 
 class Zoo:
     def __init__(self):
-        self.animal_list = [] # 동물들을 채우는 리스트
+        pass
 
     def add_animal(self, animal):
-        for e in (self.animal_list): # 한 동물씩 가져와서 동물의 이름과 종을 출력한다.
-            if (e.name == animal.name) and (e.species == animal.species):
-                print(f"{animal.name} the {animal.species}은 이미 동물원에 있습니다. 다른 이름을 사용하거나, 다른 종으로 추가해주세요.")
-                return 
-        print()
-        
-        self.animal_list.append(animal) # 동물을 추가한다.
+        pass
 
     def show_animals(self):
-        for animal in (self.animal_list): # 한 동물씩 가져와서 동물의 이름과 종을 출력한다.
-            print(f"{animal.name} the {animal.species}")
-        print()
-    
-    def show_animals_by_species(self, species):
-        for animal in (self.animal_list): # 한 동물씩 가져와서 동물의 종을 확인한다.
-            if (animal.species) == species:
-                print(f"{animal.name} the {animal.species}")
-        print()
-        
-            
+        pass
+
 class Animal:
     def __init__(self, name, species):
-        self.name = name
-        self.species = species
+        pass
 
 def main():
     # 동물원을 선언하고 10마리의 동물들을 추가합니다.
