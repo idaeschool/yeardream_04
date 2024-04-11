23d7932802d9f782a71b2432b931c9cdf173a079
hgs <t00561526@gmail.com>
2024-04-11 08:52:19 +0000

    Add prac6_hgs_test.py

--- main_hgs_test.py
+++ main_hgs_test.py
@@ -5,10 +5,11 @@ import prac2
 import prac3
 import prac4_hgs_test
 import prac5_hgs_test
+import prac6_hgs_test
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4', 'prac5'], help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', 'prac4', 'prac5', 'prac6'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -21,6 +22,8 @@ def main():
         prac4_hgs_test.read_file()
     elif args.script == 'prac5':
         prac5_hgs_test.main()
+    elif args.script == 'prac6':
+        prac6_hgs_test.game_loop()
 
 if __name__ == "__main__":
     main()
