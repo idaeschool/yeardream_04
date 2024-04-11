eb8f3918bcfa38f48ba7ed86328bfefa0ed3b9d3
jun7507 <iam_idea@naver.com>
2024-04-11 08:59:22 +0000

    main.py 수정_import name

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def main():
     elif args.script == 'prac3':
         prac3.calculate_average()
     elif args.script == 'prac4':
-        prac4.read_file()
+        prac4_양재준.read_file()
     elif args.script == 'prac5':
-        prac5.main()
+        prac5_양재준.main()
     elif args.script == 'prac6':
-        prac6.game_loop()
+        prac6_양재준.game_loop()
 
 if __name__ == "__main__":
     main()
