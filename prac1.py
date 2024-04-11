a47d99f4171f93f3d6b0cacc06ea3e5ec3e7edad
jiy.c <ji0516y@gmail.com>
2024-04-11 07:19:30 +0000

    Upload my files

--- prac1.py
+++ prac1.py
@@ -3,5 +3,5 @@
 def say_hello():
     print("Hello, World!")
 
-if __name__ == "__main__":
+if __name__ == "__main__": #== python prac1.py 로 터널에서 실행 시 (==지금 이 python script파일을 실행했다면)
     say_hello()
