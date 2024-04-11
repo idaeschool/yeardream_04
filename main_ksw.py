c34d4136c1901f652fd4f66e2c52a9047de297f6
shinwoo.kang <kang555668@gmail.com>
2024-04-11 08:50:58 +0000

    0411 practice

---
+++ main_ksw.py
@@ -0,0 +1,22 @@
+# main.py
+import argparse
+import prac1
+import prac2
+import prac3
+import prac6_kangsw
+def main():
+    parser = argparse.ArgumentParser(description="Run practice Python scripts.")
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3','prac6'], help='The script to run')
+    args = parser.parse_args()
+
+    if args.script == 'prac1':
+        prac1.say_hello()
+    elif args.script == 'prac2':
+        prac2.sum_two_numbers()
+    elif args.script == 'prac3':
+        prac3.calculate_average()
+    elif args.script == 'prac6':
+        prac6_kangsw.game_loop()
+
+if __name__ == "__main__":
+    main()
