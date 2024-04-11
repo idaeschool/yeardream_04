a46552109caa51f0a5bec65d549af780cf1e0cae
Ninezero <gygy03222@gmail.com>
2024-04-11 06:48:53 +0000

    prac4, 5 업데이트

---
+++ prac5_ninezero.py
@@ -0,0 +1,81 @@
+# prac5.py
+
+class Zoo:
+    def __init__(self):
+        self.animal_list = []
+        self.obj = []
+
+    def add_animal(self, animal):
+        if not animal in self.obj:
+            self.obj.append(animal)
+        else:
+            print(f"{animal.name}은 이미 동물원에 있습니다. 다른 이름을 사용하거나, 다른 종으로 추가해주세요.")
+
+    def show_animals(self):
+        for x in self.animal_list:
+            print(f"- {x.name} the {x.speices}")
+
+    def show_animals_by_species(self, find_spe):
+        for x in self.animal_list:
+            if x.species == find_spe:
+                print(f"- {x.name} the {x.speices}")
+    
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        
+
+def main():
+    # 동물원을 선언하고 10마리의 동물들을 추가합니다.
+    zoo = Zoo()
+    zoo.add_animal(Animal("Leo", "Lion"))
+    zoo.add_animal(Animal("Harry", "Hippo"))
+    zoo.add_animal(Animal("Ella", "Elephant"))
+    zoo.add_animal(Animal("Gerry", "Giraffe"))
+    zoo.add_animal(Animal("Gira", "Giraffe"))
+    zoo.add_animal(Animal("Terry", "Lion"))
+    zoo.add_animal(Animal("Barry", "Bear"))
+    zoo.add_animal(Animal("Larry", "Leopard"))
+    zoo.add_animal(Animal("Cary", "Crocodile"))
+    zoo.add_animal(Animal("Mary", "Monkey"))
+
+    # 동물원에 있는 모든 동물을 출력합니다.
+    zoo.show_animals()
+    ## 출력 결과 : 
+    '''
+    현재 동물원에는 다음 동물들이 있습니다 :
+    - Leo the Lion
+    - Harry the Hippo
+    - Ella the Elephant
+    - Gerry the Giraffe
+    - Gira the Giraffe
+    - Terry the Lion
+    - Barry the Bear
+    - Larry the Leopard
+    - Cary the Crocodile
+    - Mary the Monkey
+    '''
+
+    # 특정 종에 해당하는 동물들의 이름만 출력합니다.
+    zoo.show_animals_by_species("Lion")
+    # 출력 결과 : 
+    '''
+    - Leo the Lion
+    - Terry the Lion
+    '''
+    zoo.show_animals_by_species("Elephant")
+    # 출력 결과 : 
+    '''
+    - Ella the Elephant
+    '''
+
+    # 이미 존재하는 동물을 추가한 경우, 해당 동물이 이미 있음을 출력합니다.
+    zoo.add_animal(Animal("Leo", "Lion"))
+    # 출력 결과 : 
+    '''
+    "Leo the Lion"은 이미 동물원에 있습니다. 다른 이름을 사용하거나, 다른 종으로 추가해주세요.
+    '''
+
+if __name__ == "__main__":
+    main()
