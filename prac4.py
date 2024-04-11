7d5b4fb785622b9d819fd05b540602595da2d2a1
Layj <yeongjaela@gmail.com>
2024-04-11 06:42:18 +0000

    hihi

--- prac4.py
+++ prac4.py
@@ -5,10 +5,11 @@ def read_file():
     file_name을 입력받고, file_name에 해당하는 파일이 있다면, 해당 파일에 내용을 출력하는 코드를 작성하여라.
     단, 만약 파일이 존재하지 않는 경우에는 해당 에러를 예외처리하는 코드를 작성하여라.
     '''
-    file_name = 
-    try:
-        pass
-    except :
+    file_name = input()
+    try: 
+        with open(file_name, 'r') as file:
+            print(file.read())
+    except FileNotFoundError:
         pass
 
 if __name__ == "__main__":
