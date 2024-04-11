8ead59df1fbccc8aa025d0c25bc0229ab76832e1
keep4sh <keep4sh@gmail.com>
2024-04-11 08:53:27 +0000

    firsh commit

--- prac4_허성현.py
+++ prac4_허성현.py
@@ -1,15 +1,25 @@
 # prac4.py
-
+import os.path
 def read_file():
     '''
     file_name을 입력받고, file_name에 해당하는 파일이 있다면, 해당 파일에 내용을 출력하는 코드를 작성하여라.
     단, 만약 파일이 존재하지 않는 경우에는 해당 에러를 예외처리하는 코드를 작성하여라.
     '''
-    file_name = 
+    file_name = input()
+    l = []
+    folder=os.getcwd()
+
     try:
-        pass
-    except :
-        pass
+        for name in os.listdir(folder):
+            ext = name
+            l.append(ext)
+        if file_name in l:
+            print(file_name)
+        else:
+            print("There is no file in this directory.")
+    except Exception as Error:
+        print(Error)
+        # print()
 
 if __name__ == "__main__":
     read_file()
--- prac5_허성현.py
+++ prac5_허성현.py
@@ -1,22 +1,41 @@
 # prac5.py
 
 class Zoo:
-    def __init__(self):
-        pass
+    def __init__(self, zoo_dict):
+        self.zoo_dict = {}
 
     def add_animal(self, animal):
-        pass
+        if animal.name in list(self.zoo_dict.keys()) and animal.species in list(self.zoo_dict.values()):
+            print(f"{animal.name} The {animal.species}는 이미 동물원에 있습니다. 다른 이름을 사용하거나, 다른 종으로 추가해주세요.")
+        else:
+            self.zoo_dict[animal.name] = animal.species
+
 
     def show_animals(self):
-        pass
+        print("현재 동물원에는 다음 동물들이 있습니다.")
+        print()
+        for i in list(self.zoo_dict.keys()):
+            print(f" - {i} The {self.zoo_dict[i]}")
 
+    def show_animals_by_species(self, species):
+        for a, b in self.zoo_dict.items():
+            if b == species:
+               return print(f" - {a} The {b}")
+            if b not in self.zoo_dict.values():
+                return print("동물원에 말씀하신 종의 동물이 없습니다.")
+            else:
+                return print("동물원에 말씀하신 종의 동물이 없습니다.")
+        pass
+    
 class Animal:
     def __init__(self, name, species):
-        pass
+        self.name = name
+        self.species = species
 
 def main():
+    print(Animal("Leo","Lion").name)
     # 동물원을 선언하고 10마리의 동물들을 추가합니다.
-    zoo = Zoo()
+    zoo = Zoo({})
     zoo.add_animal(Animal("Leo", "Lion"))
     zoo.add_animal(Animal("Harry", "Hippo"))
     zoo.add_animal(Animal("Ella", "Elephant"))
@@ -46,13 +65,13 @@ def main():
     '''
 
     # 특정 종에 해당하는 동물들의 이름만 출력합니다.
-    zoo.show_animals_by_species("Lion")
+    # zoo.show_animals_by_species("Lion")
     # 출력 결과 : 
     '''
     - Leo the Lion
     - Terry the Lion
     '''
-    zoo.show_animals_by_species("Elephant")
+    zoo.show_animals_by_species("Parrot")
     # 출력 결과 : 
     '''
     - Ella the Elephant
