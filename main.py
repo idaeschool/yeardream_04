01f96547a7d842a64ff53147d567a1dc843398ed
Hwan <930525zz@gmail.com>
2024-04-11 15:23:07 +0900

    revise file 2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # main.py
 import argparse
-import prac1    # prac1.py를 불러오겠다!
+import prac1    # prac1.py를 
 import prac2
 import prac3
-import prac4
 
 def main():
     parser = argparse.ArgumentParser(description="Run practice Python scripts.")
-    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3', prac4], help='The script to run')
+    parser.add_argument('script', choices=['prac1', 'prac2', 'prac3'], help='The script to run')
     args = parser.parse_args()
 
     if args.script == 'prac1':
@@ -16,8 +15,6 @@ def main():
         prac2.sum_two_numbers()
     elif args.script == 'prac3':
         prac3.calculate_average()
-    elif args.script == 'prac4':
-        prac3.calculate_average()    
 
 if __name__ == "__main__":
     main()
