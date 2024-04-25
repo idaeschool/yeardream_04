6bd823e58003869d08db3053c97e9c7d7df2ce0c
crystal.gwii <crystal397@cuk.edu>
2024-04-25 05:42:27 +0000

    commit before full requests

--- prac5.py
+++ prac5.py
@@ -2,17 +2,38 @@
 
 class Zoo:
     def __init__(self):
-        pass
+        self.animals = []
 
     def add_animal(self, animal):
-        pass
+        self.animal = animal
+        if len(self.animals) != 0: # 리스트가 비어있지 않으면,
+            dup_ani = [(ani.name, ani.species) for ani in self.animals if ani.name == animal.name and ani.species == animal.species]
+            if len(dup_ani) != 0: 
+                print(f'"{dup_ani[0][0]} the {dup_ani[0][1]}"은 이미 동물원에 있습니다. 다른 이름을 사용하거나, 다른 종으로 추가해주세요.')
+            else:
+                self.animals.append(animal)
+        else:
+            self.animals.append(animal) # 리스트에 값 추가
+                       
 
     def show_animals(self):
-        pass
+        print("현재 동물원에는 다음 동물들이 있습니다 :")
+        for ani in self.animals:
+            print(f"- {ani.name} the {ani.species}")
+
+    def show_animals_by_species(self, species):
+        self.species = species
+        for ani in self.animals:
+            if ani.species == species:
+                print(f"- {ani.name} the {ani.species}")
+            else:
+                pass 
+            
 
 class Animal:
     def __init__(self, name, species):
-        pass
+        self.name = name
+        self.species = species
 
 def main():
     # 동물원을 선언하고 10마리의 동물들을 추가합니다.
